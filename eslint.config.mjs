bef2c025f30b8755379761a5d203ac9c1f6fa424
dan1m4d <daniel.madureira@ua.pt>
2025-10-14 19:05:27 +0100

    chore: update ESLint configuration and dependencies
    - Updated package.json to include new ESLint dependencies:
      - Added @eslint/eslintrc, @eslint/js, eslint, and eslint-config-prettier.
    - Changed lint script to use eslint instead of next lint.
    - Created a new eslint.config.mjs file to define ESLint configuration using FlatCompat.

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    files: ["app/**/*.{js,ts,jsx,tsx}", "lib/**/*.{js,ts,jsx,tsx}"],
+    extends: compat.extends("next/core-web-vitals", "next/typescript"),
+}]);
